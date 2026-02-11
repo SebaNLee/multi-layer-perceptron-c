6977abf3d8534864426b86f66bf0288c2ba6fc2d
SebaNLee <seba.nico.lee@gmail.com>
2026-02-10 21:40:59 -0300

    feat: add strides to tensor

--- include/mlp/tensor.h
+++ include/mlp/tensor.h
@@ -4,6 +4,7 @@ typedef struct
 {
     float * data; // buffer
     size_t * shape; // sizes per dimension
+    size_t * strides; // steps in memory
     size_t rank; // number of dimensions
     size_t size; // total number of elements
 } Tensor;
