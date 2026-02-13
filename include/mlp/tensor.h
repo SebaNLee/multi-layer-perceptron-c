37230edb4daa8d969eed0b0abc9cf8ce50b80de4
SebaNLee <seba.nico.lee@gmail.com>
2026-02-12 21:37:32 -0300

    style: pointers notation tensor.h

--- include/mlp/tensor.h
+++ include/mlp/tensor.h
@@ -1,15 +1,17 @@
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct
 {
-    float * data; // buffer
-    size_t * shape; // sizes per dimension
-    size_t * strides; // steps in memory
-    size_t rank; // number of dimensions
-    size_t size; // total number of elements
+    float *data;     // buffer
+    size_t *shape;   // sizes per dimension
+    size_t *strides; // steps in memory
+    size_t rank;     // number of dimensions
+    size_t size;     // total number of elements
 } Tensor;
 
-Tensor * tensor_new(size_t rank, size_t * shape);
-void tensor_free(Tensor * tensor);
-float tensor_get(Tensor * tensor, size_t * index);
-void tensor_set(Tensor * tensor, size_t * index, float value);
+Tensor *tensor_new(size_t rank, size_t *shape);
+void tensor_free(Tensor *t);
+float tensor_get(Tensor *t, size_t *idx);
+void tensor_set(Tensor *t, size_t *idx, float value);
