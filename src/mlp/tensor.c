dfd54f038487db6b2eb5fafca5ea0caa45dbb156
SebaNLee <seba.nico.lee@gmail.com>
2026-02-23 18:10:08 -0300

    feat: extra tensor fns implementation

--- src/mlp/tensor.c
+++ src/mlp/tensor.c
@@ -134,6 +134,90 @@ void tensor_zero(Tensor *tensor)
     memset(tensor->data, 0, tensor->size * sizeof(float));
 }
 
+Tensor *tensor_add(const Tensor *tensor1, const Tensor *tensor2)
+{
+    if (!tensor1 || !tensor2 || !tensor_same_shape(tensor1, tensor2))
+    {
+        return NULL;
+    }
+
+    Tensor *result = tensor_new(tensor1->rank, tensor1->shape);
+    if (!result)
+    {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < tensor1->size; i++)
+    {
+        result->data[i] = tensor1->data[i] + tensor2->data[i];
+    }
+
+    return result;
+}
+
+Tensor *tensor_sub(const Tensor *tensor1, const Tensor *tensor2)
+{
+    if (!tensor1 || !tensor2 || !tensor_same_shape(tensor1, tensor2))
+    {
+        return NULL;
+    }
+
+    Tensor *result = tensor_new(tensor1->rank, tensor1->shape);
+    if (!result)
+    {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < tensor1->size; i++)
+    {
+        result->data[i] = tensor1->data[i] - tensor2->data[i];
+    }
+
+    return result;
+}
+
+Tensor *tensor_mul(const Tensor *tensor1, const Tensor *tensor2)
+{
+    if (!tensor1 || !tensor2 || !tensor_same_shape(tensor1, tensor2))
+    {
+        return NULL;
+    }
+
+    Tensor *result = tensor_new(tensor1->rank, tensor1->shape);
+    if (!result)
+    {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < tensor1->size; i++)
+    {
+        result->data[i] = tensor1->data[i] * tensor2->data[i];
+    }
+
+    return result;
+}
+
+Tensor *tensor_scale(const Tensor *tensor, float scalar)
+{
+    if (!tensor)
+    {
+        return NULL;
+    }
+
+    Tensor *result = tensor_new(tensor->rank, tensor->shape);
+    if (!result)
+    {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < tensor->size; i++)
+    {
+        result->data[i] = tensor->data[i] * scalar;
+    }
+
+    return result;
+}
+
 float tensor_dot_product(const Tensor *tensor1, const Tensor *tensor2)
 {
     if (!tensor1 || !tensor2)
@@ -156,25 +240,98 @@ float tensor_dot_product(const Tensor *tensor1, const Tensor *tensor2)
     return sum;
 }
 
-Tensor *tensor_transpose_2d
+Tensor *tensor_outer_product(const Tensor *tensor1, const Tensor *tensor2)
+{
+    if (!tensor1 || !tensor2)
+    {
+        return NULL;
+    }
 
-    (const Tensor *tensor)
+    if (tensor1->rank != 1 || tensor2->rank != 1)
+    {
+        return NULL;
+    }
+
+    size_t result_shape[2] = {tensor1->shape[0], tensor2->shape[0]};
+    Tensor *result = tensor_new(2, result_shape);
+    if (!result)
+    {
+        return NULL;
+    }
+
+    size_t result_idx[2];
+
+    for (size_t i = 0; i < tensor1->shape[0]; i++)
+    {
+        for (size_t j = 0; j < tensor2->shape[0]; j++)
+        {
+            result_idx[0] = i;
+            result_idx[1] = j;
+            tensor_set(result, result_idx, tensor1->data[i] * tensor2->data[j]);
+        }
+    }
+
+    return result;
+}
+
+Tensor *tensor_matrix_multiplication(const Tensor *tensor1, const Tensor *tensor2)
+{
+    if (!tensor1 || !tensor2)
+    {
+        return NULL;
+    }
+
+    if (tensor1->rank != 2 || tensor2->rank != 2 || tensor1->shape[1] != tensor2->shape[0])
+    {
+        return NULL;
+    }
+
+    size_t result_shape[2] = {tensor1->shape[0], tensor2->shape[1]};
+    Tensor *result = tensor_new(2, result_shape);
+    if (!result)
+    {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < tensor1->shape[0]; i++)
+    {
+        for (size_t j = 0; j < tensor2->shape[1]; j++)
+        {
+            float sum = 0;
+
+            for (size_t k = 0; k < tensor1->shape[1]; k++)
+            {
+                size_t tensor1_idx[2] = {i, k};
+                size_t tensor2_idx[2] = {k, j};
+
+                sum += tensor_get(tensor1, tensor1_idx) * tensor_get(tensor2, tensor2_idx);
+            }
+
+            size_t result_idx[2] = {i, j};
+            tensor_set(result, result_idx, sum);
+        }
+    }
+
+    return result;
+}
+
+Tensor *tensor_transpose_2d(const Tensor *tensor)
 {
     if (!tensor || tensor->rank != 2)
     {
         return NULL;
     }
 
-    size_t transpose_shape[2] = {tensor->shape[1], tensor->shape[0]};
+    size_t result_shape[2] = {tensor->shape[1], tensor->shape[0]};
 
-    Tensor *transpose_tensor = tensor_new(2, transpose_shape);
-    if (!transpose_tensor)
+    Tensor *result = tensor_new(2, result_shape);
+    if (!result)
     {
         return NULL;
     }
 
     size_t tensor_idx[2];
-    size_t transpose_idx[2];
+    size_t result_idx[2];
 
     for (size_t i = 0; i < tensor->shape[0]; i++)
     {
@@ -182,12 +339,41 @@ Tensor *tensor_transpose_2d
         {
             tensor_idx[0] = i;
             tensor_idx[1] = j;
-            transpose_idx[0] = j;
-            transpose_idx[1] = i;
+            result_idx[0] = j;
+            result_idx[1] = i;
 
-            tensor_set(transpose_tensor, transpose_idx, tensor_get(tensor, tensor_idx));
+            tensor_set(result, result_idx, tensor_get(tensor, tensor_idx));
         }
     }
 
-    return transpose_tensor;
+    return result;
+}
+
+Tensor *tensor_reshape(const Tensor *tensor, size_t rank, const size_t *shape)
+{
+    if (!tensor || rank == 0 || !shape)
+    {
+        return NULL;
+    }
+
+    size_t new_size = 1;
+    for (size_t i = 0; i < rank; i++)
+    {
+        new_size *= shape[i];
+    }
+
+    if (new_size != tensor->size)
+    {
+        return NULL;
+    }
+
+    Tensor *result = tensor_new(rank, shape);
+    if (!result)
+    {
+        return NULL;
+    }
+
+    memcpy(result->data, tensor->data, new_size * sizeof(float));
+
+    return result;
 }
