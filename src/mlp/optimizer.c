4075033e6b03a25a78680150f4db5a5ee79d4f77
SebaNLee <seba.nico.lee@gmail.com>
2026-03-01 20:28:20 -0300

    feat: optimizer generic impl

--- src/mlp/optimizer.c
+++ src/mlp/optimizer.c
@@ -1,1 +1,46 @@
 #include "mlp/optimizer.h"
+#include <stdlib.h>
+
+Optimizer *optimizer_new(void *impl, const OptimizerOps *ops)
+{
+    if (!impl || !ops)
+    {
+        return NULL;
+    }
+
+    Optimizer *optimizer = malloc(sizeof(Optimizer));
+    if (!optimizer)
+    {
+        return NULL;
+    }
+
+    optimizer->impl = impl;
+    optimizer->ops = ops;
+
+    return optimizer;
+}
+
+void optimizer_step(Optimizer *optimizer, MLP *mlp)
+{
+    if (!optimizer || !mlp)
+    {
+        return;
+    }
+
+    optimizer->ops->step(optimizer, mlp);
+}
+
+void optimizer_free(Optimizer *optimizer)
+{
+    if (!optimizer)
+    {
+        return;
+    }
+
+    if (optimizer->ops && optimizer->ops->free)
+    {
+        optimizer->ops->free(optimizer);
+    }
+
+    free(optimizer);
+}
