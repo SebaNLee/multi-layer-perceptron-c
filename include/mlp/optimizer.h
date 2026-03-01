97bc8e1d695870eba6ac58092689974f7b01e904
SebaNLee <seba.nico.lee@gmail.com>
2026-03-01 20:20:25 -0300

    feat: optimizer contract

--- include/mlp/optimizer.h
+++ include/mlp/optimizer.h
@@ -1,4 +1,25 @@
 #ifndef MLP_OPTIMIZER_H
 #define MLP_OPTIMIZER_H
 
+#include "mlp/mlp.h"
+
+typedef struct Optimizer Optimizer;
+
+typedef struct
+{
+    void (*step)(Optimizer *self, MLP *mlp);
+    void (*free)(Optimizer *self);
+} OptimizerOps;
+
+struct Optimizer
+{
+    const OptimizerOps *ops;
+    void *impl;
+};
+
+// generic layer API, calls corresponding OptimizerOps *
+Optimizer *optimizer_new(void *impl, const OptimizerOps *ops);
+void optimizer_step(Optimizer *optimizer, MLP *mlp);
+void optimizer_free(Optimizer *optimizer);
+
 #endif
