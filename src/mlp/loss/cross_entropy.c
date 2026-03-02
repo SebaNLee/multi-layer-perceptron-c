c5c0457b13dcd011373472f9a0bb61208ebc44aa
SebaNLee <seba.nico.lee@gmail.com>
2026-03-02 13:10:52 -0300

    chore: loss cross entropy doxygen comments

--- src/mlp/loss/cross_entropy.c
+++ src/mlp/loss/cross_entropy.c
@@ -1,5 +1,26 @@
 #include "mlp/loss/cross_entropy.h"
 
+/**
+ * @brief Cross Entropy loss
+ *
+ * Forward input:
+ *  y_prediction
+ *  y_label
+ *
+ * Forward computes:
+ *  L = - sum_i y_label_i * log(y_prediction_i)
+ *
+ * Backward input:
+ *  y_prediction
+ *  y_label
+ *
+ * Backward computes:
+ *  dL_i = y_prediction_i - y_label_i
+ *
+ * Shapes:
+ *  y_prediction: (n, 1)
+ *  y_label: (n, 1)
+ */
 typedef struct
 {
     char _; // unused
