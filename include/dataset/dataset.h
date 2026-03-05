db07f9af04053b23e097a48626dfdf4e9745d504
SebaNLee <seba.nico.lee@gmail.com>
2026-03-05 12:53:10 -0300

    feat: dataset contract

--- include/dataset/dataset.h
+++ include/dataset/dataset.h
@@ -1,15 +1,30 @@
+#ifndef DATASET_DATASET_H
+#define DATASET_DATASET_H
+
+#include "mlp/tensor.h"
 #include <stddef.h>
 
 typedef struct
 {
-    // TODO
     size_t (*size)(Dataset *self);
     void (*shuffle)(Dataset *self);
+    bool (*next_batch)(Dataset *self, Tensor **inputs, Tensor **labels, size_t batch_size);
+    void (*reset)(Dataset *self);
     void (*free)(Dataset *self);
 } DatasetOps;
 
 typedef struct
 {
-    DatasetOps *ops;
+    const DatasetOps *ops;
     void *impl;
 } Dataset;
+
+Dataset *dataset_new(void *impl, const DatasetOps *ops);
+void dataset_free(Dataset *dataset);
+
+size_t dataset_size(Dataset *dataset);
+void dataset_shuffle(Dataset *dataset);
+bool dataset_next_batch(Dataset *dataset, Tensor **inputs, Tensor **lables, size_t batch_size);
+void dataset_reset(Dataset *dataset);
+
+#endif
