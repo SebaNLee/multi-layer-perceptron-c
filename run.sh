be1033c31985ccfa7b0db1effa9d8fcbccf84912
SebaNLee <seba.nico.lee@gmail.com>
2026-03-07 09:56:37 -0300

    build!: run.sh relase and debug, optimizations too

--- run.sh
+++ run.sh
@@ -2,14 +2,27 @@
 
 set -e
 
+MODE=$1
+EXAMPLE=$2
+
 # clean
 rm -rf ./build
+mkdir build
+
+# configure CMake mode
+cmake -S . -B build/debug -DCMAKE_BUILD_TYPE=Debug
+cmake -S . -B build/release -DCMAKE_BUILD_TYPE=Release
 
 # compile
-mkdir build
-cd build
-cmake ..
-make
+cmake --build build/debug
+cmake --build build/release
 
 # run
-./examples/$1
+if [ "$MODE" = "debug" ]; then
+    ./build/debug/examples/$EXAMPLE
+elif [ "$MODE" = "release" ]; then
+    ./build/release/examples/$EXAMPLE
+else
+    echo "Usage: ./run.sh [debug|release] <example>"
+    exit 1
+fi
