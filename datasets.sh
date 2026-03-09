9adb360e726ad2996cd9fe0f13555dac71ebd4b0
SebaNLee <seba.nico.lee@gmail.com>
2026-03-09 01:27:55 -0300

    build: idem modularization

--- datasets.sh
+++ datasets.sh
@@ -12,43 +12,42 @@ if [ "$TARGET" != "all" ] && [ "$TARGET" != "emnist" ] && [ "$TARGET" != "mushro
     exit 1
 fi
 
-# download
+# helper
+wget_unzip() {
+  local name="$1"
+  local url="$2"
+  local zip="${name}.zip"
+  local dir="$DATASETS_DIR/$name"
+  rm -rf "$dir"
+  mkdir -p "$dir"
+  (
+    cd "$dir"
+    wget -q --show-progress "$url" -O "$zip"
+    unzip -q -j -o "$zip"
+    rm -f "$zip"
+  )
+}
+
+# download and process
 if [ "$TARGET" = "all" ] || [ "$TARGET" = "emnist" ]; then
-    EMNIST_DIR="$DATASETS_DIR/emnist"
-    EMNIST_URL="https://biometrics.nist.gov/cs_links/EMNIST/gzip.zip"
-    rm -rf "$EMNIST_DIR"
-    mkdir -p "$EMNIST_DIR"
-    cd "$EMNIST_DIR"
-    wget -q --show-progress "$EMNIST_URL" -O emnist.zip
-    unzip -q -j emnist.zip
-    rm emnist.zip
-    rm !(emnist-digits-*|emnist-letters-*)
-    gunzip -f *.gz
+    wget_unzip "emnist" "https://biometrics.nist.gov/cs_links/EMNIST/gzip.zip"
+    (
+        cd "$DATASETS_DIR/emnist"
+        rm -f !(emnist-digits-*|emnist-letters-*)
+        gunzip -f *.gz
+    )
 fi
 if [ "$TARGET" = "all" ] || [ "$TARGET" = "mushroom" ]; then
-    MUSHROOM_DIR="$DATASETS_DIR/mushroom"
-    MUSHROOM_URL="https://archive.ics.uci.edu/static/public/73/mushroom.zip"
-    rm -rf "$MUSHROOM_DIR"
-    mkdir -p "$MUSHROOM_DIR"
-    cd "$MUSHROOM_DIR"
-    wget -q --show-progress "$MUSHROOM_URL" -O mushroom.zip
+    wget_unzip "mushroom" "https://archive.ics.uci.edu/static/public/73/mushroom.zip"
 
 fi
 if [ "$TARGET" = "all" ] || [ "$TARGET" = "shopper" ]; then
-    SHOPPER_DIR="$DATASETS_DIR/shopper"
-    SHOPPER_URL="https://archive.ics.uci.edu/static/public/468/online+shoppers+purchasing+intention+dataset.zip"
-    rm -rf "$SHOPPER_DIR"
-    mkdir -p "$SHOPPER_DIR"
-    cd "$SHOPPER_DIR"
-    wget -q --show-progress "$SHOPPER_URL" -O shopper.zip
+    wget_unzip "shopper" "https://archive.ics.uci.edu/static/public/468/online+shoppers+purchasing+intention+dataset.zip"
 
 fi
 if [ "$TARGET" = "all" ] || [ "$TARGET" = "heart" ]; then
-    HEART_DIR="$DATASETS_DIR/heart"
-    HEART_URL="https://archive.ics.uci.edu/static/public/45/heart+disease.zip"
-    rm -rf "$HEART_DIR"
-    mkdir -p "$HEART_DIR"
-    cd "$HEART_DIR"
-    wget -q --show-progress "$HEART_URL" -O heart.zip
+    wget_unzip "heart" "https://archive.ics.uci.edu/static/public/45/heart+disease.zip"
 
 fi
+
+
