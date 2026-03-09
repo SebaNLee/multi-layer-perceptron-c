35c72486370f99e9fe0f90fc60b7e2aa3deeeaca
SebaNLee <seba.nico.lee@gmail.com>
2026-03-09 01:02:56 -0300

    build: some UCI datasets download

--- datasets.sh
+++ datasets.sh
@@ -2,18 +2,19 @@
 
 set -e
 shopt -s extglob
-ROOT_DIR=$(pwd)
+ROOT_DIR="$(cd "$(dirname "$0")" && pwd)"
+DATASETS_DIR="$ROOT_DIR/datasets"
 TARGET=$1
 
 # args validation
-if [ "$TARGET" != "all" ] && [ "$TARGET" != "emnist" ]; then
-    echo "Usage: ./datasets.sh [all|emnist]"
+if [ "$TARGET" != "all" ] && [ "$TARGET" != "emnist" ] && [ "$TARGET" != "mushroom" ] && [ "$TARGET" != "shopper" ] && [ "$TARGET" != "heart" ]; then
+    echo "Usage: ./datasets.sh [all|emnist|mushroom|shopper|heart]"
     exit 1
 fi
 
 # download
 if [ "$TARGET" = "all" ] || [ "$TARGET" = "emnist" ]; then
-    EMNIST_DIR="$ROOT_DIR/datasets/emnist"
+    EMNIST_DIR="$DATASETS_DIR/emnist"
     EMNIST_URL="https://biometrics.nist.gov/cs_links/EMNIST/gzip.zip"
     rm -rf "$EMNIST_DIR"
     mkdir -p "$EMNIST_DIR"
@@ -24,6 +25,30 @@ if [ "$TARGET" = "all" ] || [ "$TARGET" = "emnist" ]; then
     rm !(emnist-digits-*|emnist-letters-*)
     gunzip -f *.gz
 fi
-# if [ "$TARGET" = "all" ] || [ "$TARGET" = "" ]; then
-#     # TODO
-# fi
+if [ "$TARGET" = "all" ] || [ "$TARGET" = "mushroom" ]; then
+    MUSHROOM_DIR="$DATASETS_DIR/mushroom"
+    MUSHROOM_URL="https://archive.ics.uci.edu/static/public/73/mushroom.zip"
+    rm -rf "$MUSHROOM_DIR"
+    mkdir -p "$MUSHROOM_DIR"
+    cd "$MUSHROOM_DIR"
+    wget -q --show-progress "$MUSHROOM_URL" -O mushroom.zip
+
+fi
+if [ "$TARGET" = "all" ] || [ "$TARGET" = "shopper" ]; then
+    SHOPPER_DIR="$DATASETS_DIR/shopper"
+    SHOPPER_URL="https://archive.ics.uci.edu/static/public/468/online+shoppers+purchasing+intention+dataset.zip"
+    rm -rf "$SHOPPER_DIR"
+    mkdir -p "$SHOPPER_DIR"
+    cd "$SHOPPER_DIR"
+    wget -q --show-progress "$SHOPPER_URL" -O shopper.zip
+
+fi
+if [ "$TARGET" = "all" ] || [ "$TARGET" = "heart" ]; then
+    HEART_DIR="$DATASETS_DIR/heart"
+    HEART_URL="https://archive.ics.uci.edu/static/public/45/heart+disease.zip"
+    rm -rf "$HEART_DIR"
+    mkdir -p "$HEART_DIR"
+    cd "$HEART_DIR"
+    wget -q --show-progress "$HEART_URL" -O heart.zip
+
+fi
